e33d21bbb35e08dee0d3d40518c8ac7b9c074b52
Suphi Bayrak <Suphi Bayrak@SUPHI-PC>
2021-12-20 21:45:46 +0300

    Contact api proje eklendi.

---
+++ ContactReportApp.ContactApi/Controllers/RehberKisiController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContactReportApp.ContactApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class RehberKisiController : ControllerBase
+    {
+        private readonly ILogger<RehberKisiController> _logger;
+        public RehberKisiController(ILogger<RehberKisiController> logger)
+        {
+            _logger = logger;
+        }
+        [HttpGet]
+        public List<string> Get()
+        {
+            return new List<string>() { "1", "2", "3" };
+        }
+    }
+}
